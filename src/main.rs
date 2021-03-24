4cc99865738517f3a5b6b001baf357524c32def3
Seth Stadick <sstadick@gmail.com>
2021-03-24 08:10:17 -0600

    1. A Freestanding Rust Binary

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+#![no_std] // don't link the Rust standard library
+#![no_main] // disable Rust-level entry-points
+
+use core::panic::PanicInfo;
+
+/// This function is called on panic
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle] // don't mangle the name of this function
+pub extern "C" fn _start() -> ! {
+    // this function is the enry point, since the linker looks for a function
+    // named `_start` by default. Normally this would be the C runtime...?
+    loop {}
+}
+
